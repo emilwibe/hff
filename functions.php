9d490e0427d5fde69ecd704267eb00e0744e6b81
Emil Wibe <ewweb@EWWeb-Max.local>
2022-03-14 10:59:32 +0100

    [setup] register menus

--- functions.php
+++ functions.php
@@ -16,3 +16,14 @@ add_action('wp_enqueue_scripts', 'hff_enqueues');
 function hff_enqueues(){
     wp_enqueue_style('main-styles', get_template_directory_uri() . '/dist/core.min.css');
 }
+/**
+ * REGISTER MENUS
+ *
+*/
+add_action('init', 'hff_add_menus');
+
+function hff_add_menus() {
+    register_nav_menus(array(
+        'nav_primary' => 'Primary'
+    ));
+}
