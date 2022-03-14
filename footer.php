03856fc264c3d6e253ca6708d649519820db4a77
Emil Wibe <ewweb@EWWeb-Max.local>
2022-03-14 13:34:24 +0100

    styles

--- footer.php
+++ footer.php
@@ -1,6 +1,11 @@
 <footer class="footer primary">
     <div class="l-wrapper xl">
-        
+    
+    <?php
+    if ( get_field('logo_in_footer') ) {
+        echo get_field('logo_in_footer');
+    }
+    ?>
     <?php get_template_part('template-parts/logo-bottom'); ?>
 
     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar("Sidefod") ) : ?>
