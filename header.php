9ec6077303d67433c18ffc243e8144a8beca014e
Emil Wibe <ewweb@EWWeb-Max.local>
2022-03-16 11:03:10 +0100

    styles

--- header.php
+++ header.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php echo bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 
-<?php wp_body_open(); ?>
+    <?php wp_body_open(); ?>
+
+    <header class="header primary">
+        <div class="l-wrapper xl">
+            <a href="<?php echo get_site_url(); ?>">Logo</a>
+
+            <?php get_template_part( 'template-parts/nav-primary' ); ?>
+        </div>
+    </header>
