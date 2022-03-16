8a4caacba0ba6afbac31fea906704f977fcaada0
Emil Wibe <ewweb@EWWeb-Max.local>
2022-03-16 11:41:10 +0100

    styles

--- template-parts/post-thumbnail.php
+++ template-parts/post-thumbnail.php
@@ -3,5 +3,6 @@
         $attachment_id = get_post_thumbnail_id();
         $img_src = wp_get_attachment_image_url( $attachment_id, 'full');
         $img_srcset = wp_get_attachment_image_srcset( $attachment_id, 'medium' );
-?>
-        <img src="<?php echo esc_url( $img_src ); ?>" srcset="<?php echo esc_attr( $img_srcset ); ?>" sizes="(max-width: 50em) 87vw, 680px" alt="Foo Bar">
+
+        the_post_thumbnail( 'full' );
+    }
