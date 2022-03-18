55b67cbd186622684a9adf486798d0b6b495eede
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-18 11:18:35 +0100

    styles

---
+++ ct_fw.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Fuld Bredde
+ * Template Post Type: post, page
+ */
+?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
---
+++ ct_right_col.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Template Name: Højre kolonne
+ * Template Post Type: post, page
+ */
+?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
