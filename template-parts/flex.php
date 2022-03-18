8e2401dd0365d2977f4f1dad842abc215b394c0c
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-18 12:58:46 +0100

    styles

--- template-parts/flex.php
+++ template-parts/flex.php
@@ -1,6 +1,6 @@
 <?php if ( have_rows('hff_flex_content') ) : ?>
 
-    <?php while( have_rows('hff_flex_content') ) : ?>
+    <?php while( have_rows('hff_flex_content') ) : the_row(); ?>
 
         <?php
             if(get_row_layout == 'hff_img_wysiwyg') {
