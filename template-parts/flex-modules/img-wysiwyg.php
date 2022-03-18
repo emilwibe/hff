c20f96d4c46685a80cf61c05309422ec04200fda
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-18 13:03:39 +0100

    styles

--- template-parts/flex-modules/img-wysiwyg.php
+++ template-parts/flex-modules/img-wysiwyg.php
@@ -1,1 +1,6 @@
-Hello flex
+<div class="hdd-mod hdd-mod-img-wysiwyg">
+    <?php if (get_subfield('hff_flex_heading')) : ?>
+
+        <h2><?php the_subfield('hff_flex_heading'); ?></h2>
+    <?php endif; ?>
+</div>
