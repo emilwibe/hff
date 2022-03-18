5e5a238540c8e4ba70066f3cf1d0f99f00960493
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-18 13:05:19 +0100

    styles

--- template-parts/flex-modules/img-wysiwyg.php
+++ template-parts/flex-modules/img-wysiwyg.php
@@ -3,4 +3,9 @@
 
         <h2><?php the_sub_field('hff_flex_heading'); ?></h2>
     <?php endif; ?>
+
+    <?php if (get_sub_field('hff_section_heading')) : ?>
+
+<h2><?php the_sub_field('hff_section_heading'); ?></h2>
+<?php endif; ?>
 </div>
