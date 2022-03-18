312f62252b8deff4b721d27238e163848bcb2244
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-18 13:04:10 +0100

    styles

--- template-parts/flex-modules/img-wysiwyg.php
+++ template-parts/flex-modules/img-wysiwyg.php
@@ -1,6 +1,6 @@
 <div class="hdd-mod hdd-mod-img-wysiwyg">
-    <?php if (get_subfield('hff_flex_heading')) : ?>
+    <?php if (get_sub_field('hff_flex_heading')) : ?>
 
-        <h2><?php the_subfield('hff_flex_heading'); ?></h2>
+        <h2><?php the_sub_field('hff_flex_heading'); ?></h2>
     <?php endif; ?>
 </div>
