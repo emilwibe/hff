a3d75c6b36ea00785f69f188188a6a1efb2bc579
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-18 11:23:16 +0100

    styles

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php get_header(); ?>
 
-<?php if(have_posts()) : ?>
-    <?php while(have_posts()) : the_post(); ?>
 
-    <?php echo get_page_template_slug(); ?>
-
-    <?php endwhile; ?>
-
-<?php endif; ?>
 
 <?php get_footer(); ?>
