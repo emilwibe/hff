cf719256feb5c3160f372164e8a39ad7900fc48f
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-20 18:01:27 +0100

    styles

--- template-parts/flex-modules/img.php
+++ template-parts/flex-modules/img.php
@@ -1,9 +1,9 @@
 <div class="hff-mod hff-mod-img">
 
-<?php if (get_sub_field('hff_wysiwyg_singular')) : ?>
+<?php if (get_sub_field('hff_img_singular')) : ?>
 
     <?php
-        $imageID = get_sub_field('hff_wysiwyg_singular');
+        $imageID = get_sub_field('hff_img_singular');
 
         echo wp_get_attachment_image($imageID, 'full', false, ['class' => 'primary-img fade-in-on-load', 'onload' => 'loadImageonLoad (this)']);
     ?>
