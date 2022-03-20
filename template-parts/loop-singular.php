49aed80e087669700e4793c3ba1a5feedc390e75
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-20 18:10:53 +0100

    styles

--- template-parts/loop-singular.php
+++ template-parts/loop-singular.php
@@ -1,6 +1,6 @@
 <?php if (have_posts()) : ?>
 
-    <div class="l-wrapper l grid lg-68-32">
+    < class="l-wrapper l grid lg-68-32">
     <?php while(have_posts()) : the_post(); ?>
 
     <main>
@@ -11,9 +11,7 @@
         <?php get_template_part('template-parts/flex'); ?>
     </main>
 
-    <aside>
-
-    </aside>
+        <?php get_template_part('template-parts/flex-sidebar'); ?>
 
     <?php endwhile; ?>
 
