e7eb5718a7b4fec24a2c1934700619dc7a836eb4
Emil Wibe <ewweb@ewweb-max.lan>
2022-03-20 15:07:59 +0100

    styles

--- front-page.php
+++ front-page.php
@@ -1,31 +1,5 @@
 <?php get_header(); ?>
 
-<?php if (have_posts()) : ?>
-
-    <main>
-        
-            <?php while (have_posts()) : the_post(); ?>
-
-            <div class="l-wrapper l">
-
-                <div class="grid index-introduction">
-
-                    <?php get_template_part('template-parts/logo-index'); ?>
-
-                    <div class="the_content wysiwyg">
-                        <?php the_content(); ?>
-                    </div>
-
-                </div>
-                
-            </div><!--/.l-wrapper-->
-
-                <?php get_template_part( 'template-parts/flex' ); ?>
-
-            <?php endwhile; ?>
-
-    </main>
-
-<?php endif; ?>
+<?php get_template_part('template-parts/loop-index'); ?>
 
 <?php get_footer(); ?>
---
+++ template-parts/loop-index.php
@@ -0,0 +1,27 @@
+<?php if (have_posts()) : ?>
+
+<main>
+    
+        <?php while (have_posts()) : the_post(); ?>
+
+        <div class="l-wrapper l">
+
+            <div class="grid index-introduction">
+
+                <?php get_template_part('template-parts/logo-index'); ?>
+
+                <div class="the_content wysiwyg">
+                    <?php the_content(); ?>
+                </div>
+
+            </div>
+            
+        </div><!--/.l-wrapper-->
+
+            <?php get_template_part( 'template-parts/flex' ); ?>
+
+        <?php endwhile; ?>
+
+</main>
+
+<?php endif; ?>
