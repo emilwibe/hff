151b1731ca98520e433b7bb7e66c0d852a8bfb5b
Emil Wibe <ewweb@EWWeb-Max.local>
2022-03-22 13:43:19 +0100

    styles

--- template-parts/loop-singular.php
+++ template-parts/loop-singular.php
@@ -1,6 +1,13 @@
 <?php if (have_posts()) : ?>
 
-    <div class="l-wrapper l grid lg-68-32">
+    <?php
+        $colored_types = get_field('hff_strong_types_colored');
+        if ($colored_types[0] == 'true') {
+            $colred_types = "colored-types";
+        }
+    ?>
+
+    <div class="l-wrapper l grid lg-68-32 <?php echo $colred_types; ?>">
     <?php while(have_posts()) : the_post(); ?>
 
     <main>
