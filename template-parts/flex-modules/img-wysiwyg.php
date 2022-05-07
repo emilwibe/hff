9a829c9055df6f6ca563373d1ba2aff60fa6a7b2
Emil Wibe <ewweb@ewweb-max.lan>
2022-05-07 09:48:43 +0200

    styles

--- template-parts/flex-modules/img-wysiwyg.php
+++ template-parts/flex-modules/img-wysiwyg.php
@@ -1,4 +1,4 @@
-<div class="l-wrapper xl hff-mod hff-mod-img-wysiwyg">
+<div class="l-wrapper xl hff-mod hff-mod-img-wysiwyg hff-spacing-<?php the_sub_field('hff_full_width_content'); ?>">
     <?php if (get_sub_field('hff_section_heading')) : ?>
 
     <div class="grid grid-16">
